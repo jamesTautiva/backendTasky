1f473cc3637ecc6aa5735fa18d9ab3c2ac122763
jamesTautiva <152186922+jamesT-ctr@users.noreply.github.com>
2024-12-13 15:21:45 -0500

    new commit

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const User = require('../models/userModels')
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken')
 const jwtSecret = "contraseña secreta";
 const nodemailer = require('nodemailer');
