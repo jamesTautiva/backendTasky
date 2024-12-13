3c6243e1dcac636baa607b637bb56d175569047c
jamesTautiva <152186922+jamesT-ctr@users.noreply.github.com>
2024-12-13 14:52:05 -0500

    act

--- src/config/db.js
+++ src/config/db.js
@@ -3,13 +3,14 @@ const {Sequelize} = require("sequelize");
 
 //conetion mysql
 
-const sequelize = new Sequelize('trello','root', '0405',{
-    hostname: 'localhost',
-    dialect: 'mysql',
-    loggin: false,
-})
-
-
+const sequelize = new Sequelize({
+  host: "34.29.209.142",         // Puede ser IP pública o privada de Cloud SQL
+  dialect: 'mysql',
+  username: "james tautiva",     // Usuario de la base de datos
+  password: "0405", // Contraseña de la base de datos
+  database: "tasky",     // Nombre de la base de datos
+  logging: false,                    // Opcional: Desactivar logs SQL
+});
 sequelize.authenticate()
 .then(() => {
     console.log('conecteted to database')
@@ -18,7 +19,7 @@ sequelize.authenticate()
     console.log('error conected to database',err)
 })
 
-sequelize.sync({ force: true })  // Si deseas eliminar las tablas, pon `force: true`
+sequelize.sync({ force: false })  // Si deseas eliminar las tablas, pon `force: true`
   .then(() => {
     console.log('Tablas sincronizadas');
   })
